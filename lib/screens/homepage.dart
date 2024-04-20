5bd6b57be15763edbb12cb4b8f6e4c16171dc9a9
Alto b <altob282@gmail.com>
2024-04-20 18:15:35 +0530

    animation updated

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,130 +1,172 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
-
-import 'package:crypto/crypto.dart';
+import 'package:SecurePass/screens/scanner.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:lottie/lottie.dart';
 import 'package:flutter/services.dart';
+import 'package:crypto/crypto.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:lottie/lottie.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:screenshot/screenshot.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   final _key = GlobalKey<FormState>();
   TextEditingController textController = TextEditingController();
+  ScreenshotController screenshotController = ScreenshotController();
 
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      // appBar: AppBar(
-      //   backgroundColor: Colors.grey[300],
-      // ),
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("SecurePass",style: TextStyle(
+            color: Colors.black54,
+            fontWeight: FontWeight.w800,
+            fontSize: 15
+          ),),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(onPressed: (){
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => ScannerPage(),));
+              Navigator.of(context).push(_routeToScanner());
+            }, icon: Icon(Icons.qr_code_scanner_outlined)),
+          )
+        ],
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Center(
             child: GestureDetector(
               onTap: () {
-                 FocusScope.of(context).unfocus();
+                FocusScope.of(context).unfocus();
               },
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(height: screenHeight/9,),
-                  textController.text.trim().length == 0 ?
-                  Lottie.asset('lib/assets/pg.json',height: screenHeight/3) :
-                  QrImageView(
-                      data: textController.text.trim(),
-                      version: QrVersions.auto,
-                      size: screenHeight/3,
-                    ),
-                  const SizedBox(height: 20,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                     textController.text.isEmpty?Text("Enter something!",style: textStyle1(),) :
-                     Expanded(
-                       child: Center(
-                         child: Text(generatePasswordFromText(textController.text, textController.text.length),
-                          // style: textController.text.length>=8? passwordStyle().copyWith(color: Colors.green) : passwordStyle(),
-                          style: passwordStyle(textController.text.length),
-                          ),
-                       ),
-                     ),
-                      textController.text.isEmpty?SizedBox() : 
-                      PlatformIconButton(
-                    onPressed: () async{
-                      await Clipboard.setData(ClipboardData(text: generatePasswordFromText(textController.text, textController.text.length).trim()));
-                    },
-                    icon: Icon(Icons.copy),
-                    material: (context, platform) => MaterialIconButtonData(
-                      autofocus: true,
-                      color: Colors.blueAccent
-                    ),
-                    cupertino: (context, platform) => CupertinoIconButtonData(
-                      
+                  SizedBox(height: screenHeight / 12),
+                  Screenshot(
+                    controller: screenshotController,
+                    child: Column(
+                      children: [
+                        textController.text.trim().length == 0 ?
+                        Lottie.asset('lib/assets/pg.json',height: screenHeight/3) :
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            QrImageView(
+                                data: textController.text.trim(),
+                                version: QrVersions.auto,
+                                size: screenHeight/4,
+                              ), 
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            textController.text.isEmpty
+                                ? Text(
+                                    "Enter something!",
+                                    style: textStyle1(),
+                                  )
+                                : Expanded(
+                                    child: Center(
+                                      child: Text(
+                                        generatePasswordFromText(
+                                          textController.text,
+                                          textController.text.length,
+                                        ),
+                                        style: passwordStyle(
+                                            textController.text.length),
+                                      ),
+                                    ),
+                                  ),
+                            textController.text.isEmpty
+                                ? SizedBox()
+                                : Row(
+                                  children: [
+                                    IconButton(
+                                        onPressed: () async {
+                                          await Clipboard.setData(
+                                            ClipboardData(
+                                              text: generatePasswordFromText(
+                                                      textController.text,
+                                                      textController.text.length)
+                                                  .trim(),
+                                            ),
+                                          );
+                                        },
+                                        icon: Icon(Icons.copy),
+                                        color: Colors.blueAccent,
+                                      ),
+
+                                      IconButton(onPressed: () async {
+                                Uint8List? imageBytes =
+                          await screenshotController.capture();
+                      await saveQrCodeImage(imageBytes!);
+                              }, icon: Icon(Icons.save_alt_outlined,color: Colors.cyan))
+                                  ],
+                                ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                    ],
-                  ),
-                  SizedBox(height: 20,),
-                  // Spacer(),
+                  SizedBox(height: 20),
                   Form(
                     key: _key,
                     child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Card(
-                          color: Colors.grey.shade100,
-                          elevation:2,
-                          child: TextFormField(
-                            textAlign: TextAlign.center,
-                            controller: textController,
-                            decoration: InputDecoration(
-                              suffix: Text(textController.text.length.toString(),style: TextStyle(
-                                color: Colors.grey[500]
-                              ),),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(25),
-                                borderSide: BorderSide.none
-                              )
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Card(
+                            color: Colors.grey.shade100,
+                            elevation: 2,
+                            child: TextFormField(
+                              textAlign: TextAlign.center,
+                              controller: textController,
+                              decoration: InputDecoration(
+                                prefix: Text(
+                                  textController.text.length.toString(),
+                                  style: TextStyle(color: Colors.grey[500]),
+                                ),
+                                suffix: textController.text.length != 0 ? IconButton(onPressed: () {
+                                  textController.clear();
+                                  setState(() {});
+                                }, icon: Icon(Icons.clear,color: Colors.grey,)) : SizedBox(),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                    borderSide: BorderSide.none),
+                              ),
+                              onChanged: (value) {
+                                generatePasswordFromText(
+                                  textController.text,
+                                  textController.text.length,
+                                );
+                                setState(() {});
+                              },
                             ),
-                            onChanged: (value) {
-                              generatePasswordFromText(textController.text, textController.text.length);
-                              setState(() {});
-                            },
                           ),
                         ),
-                      ),
-                      
-                      
-                    ],
-                  )),
-                  SizedBox(height: 30,),
-          
-                  // PlatformIconButton(
-                  //   onPressed: () {
-                      
-                  //   },
-                  //   icon: Icon(Icons.qr_code),
-                  //   material: (context, platform) => MaterialIconButtonData(
-                  //     autofocus: true
-                  //   ),
-                  //   cupertino: (context, platform) => CupertinoIconButtonData(
-                      
-                  //   ),
-                  // ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -135,131 +177,103 @@ class _HomePageState extends State<HomePage> {
   }
 
   TextStyle passwordStyle(int length) {
-  Color textColor;
-  if (length <=2) {
-    textColor = Colors.red;
-  }
-  else if (length>=5 && length<=7){
-    textColor = Colors.amber;
-  }
-  else if (length>=2 && length<=4){
-    textColor = Colors.orange;
-  }
-  else if (length >= 8) {
-    textColor = Colors.green;
-  }
-   else {
-    textColor = Colors.grey;
+    Color textColor;
+    if (length <= 2) {
+      textColor = Colors.red;
+    } else if (length >= 5 && length <= 7) {
+      textColor = Colors.amber;
+    } else if (length >= 2 && length <= 4) {
+      textColor = Colors.orange;
+    } else if (length >= 8) {
+      textColor = Colors.green;
+    } else {
+      textColor = Colors.grey;
+    }
+
+    return TextStyle(
+      fontSize: 25,
+      color: textColor,
+      fontWeight: FontWeight.w700,
+    );
   }
 
-  return TextStyle(
-    fontSize: 25,
-    color: textColor,
-    fontWeight: FontWeight.w700,
-  );
-}
-
-
-  TextStyle textStyle1() =>TextStyle(
-    fontSize: 25,
-    color: Colors.grey[500],
-    fontWeight: FontWeight.w700
-  );
+  TextStyle textStyle1() => TextStyle(
+      fontSize: 25, color: Colors.grey[500], fontWeight: FontWeight.w700);
 
   String generatePasswordFromText(String text, int length) {
-  // Convert the input text to bytes and hash it
-  final bytes = utf8.encode(text);
-  final hash = sha256.convert(bytes).toString();
+    final bytes = utf8.encode(text);
+    final hash = sha256.convert(bytes).toString();
 
-  // Characters for the password generation
-  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const symbols = '!@#%^&*'; // Symbols to include when text length >= 8
-  const numbers = '0123456789'; // Numbers to include when text length >= 8
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#%^&*';
+    const numbers = '0123456789';
 
-  // Use the hash as a seed for the random number generator
-  final random = Random(hash.hashCode);
+    final random = Random(hash.hashCode);
 
-  // Generate the password based on the hash
-  String password = String.fromCharCodes(Iterable.generate(
-      length, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
+    String password = String.fromCharCodes(Iterable.generate(
+        length, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
 
-  // Include a symbol if the text length is greater than or equal to 8
-  if (text.length >= 8) {
-    final symbol = symbols[random.nextInt(symbols.length)];
-    final number = numbers[random.nextInt(numbers.length)];
+    if (text.length >= 8) {
+      final symbol = symbols[random.nextInt(symbols.length)];
+      final number = numbers[random.nextInt(numbers.length)];
 
-    // Insert the symbol at a random position in the password
-    final index = random.nextInt(password.length + 1);
-    password = password.substring(0, index) + symbol + password.substring(index);
+      final index = random.nextInt(password.length + 1);
+      password =
+          password.substring(0, index) + symbol + password.substring(index);
 
-    // Insert the number at a random position in the password
-    final index2 = random.nextInt(password.length + 1);
-    password = password.substring(0, index2) + number + password.substring(index2);
+      final index2 = random.nextInt(password.length + 1);
+      password = password.substring(0, index2) +
+          number +
+          password.substring(index2);
+    }
+
+    return password;
   }
 
-  return password;
+  Future<void> saveQrCodeImage(Uint8List imageBytes) async {
+    final result = await ImageGallerySaver.saveImage(imageBytes);
+    print(result);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('QR Code saved to gallery.'),
+      ),
+    );
+  }
+
+//   Future<void> _captureAndSharePng() async {
+//   screenshotController.capture().then((File image1) async {
+//     //Capture Done
+//     List<int> bytes1 = await image1.readAsBytes();
+
+//     await Share.file("Code",
+//       'title' + ".jpg", bytes1, 'image/jpg',
+//       text: "Join Group by scanning this code"
+//     );
+//   } as FutureOr Function(Uint8List? value)).catchError((onError) {
+//     print(onError);
+//   });
+// }
 }
 
+Route _routeToScanner() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const ScannerPage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+  const begin = Offset(0.0, 1.0);
+  const end = Offset.zero;
+  const curve = Curves.ease;
 
-//   String generatePasswordFromText(String text, int length) {
-//   // Convert the input text to bytes and hash it
-//   final bytes = utf8.encode(text);
-//   final hash = sha256.convert(bytes).toString();
+  final tween = Tween(begin: begin, end: end);
+  final curvedAnimation = CurvedAnimation(
+    parent: animation,
+    curve: curve,
+  );
 
-//   // Characters for the password generation
-//   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-//   const symbols = '!@#%^&*'; // Symbols to include when text length >= 8
+  return SlideTransition(
+    position: tween.animate(curvedAnimation),
+    child: child,
+  );
 
-//   // Use the hash as a seed for the random number generator
-//   final random = Random(hash.hashCode);
-
-//   // Generate the password based on the hash
-//   String password = String.fromCharCodes(Iterable.generate(
-//       length, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
-
-//   // Include a symbol if the text length is greater than or equal to 8
-//   if (text.length >= 8) {
-//     final symbol = symbols[random.nextInt(symbols.length)];
-//     // Insert the symbol at a random position in the password
-//     final index = random.nextInt(password.length + 1);
-//     password = password.substring(0, index) + symbol + password.substring(index);
-//   }
-
-//   return password;
-// }
-
-
-// String generatePasswordFromText(String text, int length) {
-//   // If the text is empty, generate a completely random password
-//   if (text.length ==0) {
-//     return generateRandomPassword(length);
-//   } else {
-//     // Convert the input text to bytes and hash it
-//     final bytes = utf8.encode(text);
-//     final hash = sha256.convert(bytes).toString();
-
-//     // Characters for the password generation
-//     const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%^&*';
-
-//     // Use the hash as a seed for the random number generator
-//     final random = Random(hash.hashCode);
-
-//     // Generate the password based on the hash
-//     return String.fromCharCodes(Iterable.generate(
-//         length, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
-//   }
-// }
-
-// String generateRandomPassword(int length) {
-//   // Characters for the password generation
-//   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%^&*';
-
-//   // Use a random number generator
-//   final random = Random();
-
-//   // Generate a completely random password
-//   return String.fromCharCodes(Iterable.generate(
-//       length, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
-// }
-
+},
+  );
 }
---
+++ lib/screens/scanner.dart
@@ -0,0 +1,162 @@
+import 'package:any_link_preview/any_link_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_link_previewer/flutter_link_previewer.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class ScannerPage extends StatefulWidget {
+  const ScannerPage({super.key});
+
+  @override
+  State<ScannerPage> createState() => _ScannerPageState();
+}
+
+class _ScannerPageState extends State<ScannerPage> {
+
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        title: Text("SecurePass",style: TextStyle(
+          color: Colors.black54,
+          fontWeight: FontWeight.w800,
+          fontSize: 15
+        ),),
+      ),
+    body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              overlay: QrScannerOverlayShape(
+                overlayColor: Colors.black38,
+                borderWidth: 10,
+                borderRadius: 10,
+                borderColor: (Colors.red.shade500),
+              ),
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            // child: Center(
+            //   child: (result != null)
+            //       ? isValidWebLink(result!.code!) ?
+            //        AnyLinkPreview(link: result!.code!,
+            //        )
+            //        : Text('${result!.code}')
+            //       : Text('Scan a code'),
+            // ),
+            child: Center(
+                    child: () {
+                      if (result != null) {
+                        if (isValidWebLink(result!.code!)) {
+                          // return AnyLinkPreview(link: result!.code!,backgroundColor: Colors.red,);
+                          // return Text("valid link");
+                          return Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: AnyLinkPreview(
+                                        link: result!.code!,
+                                        displayDirection: UIDirection.uiDirectionHorizontal, 
+                                        cache: const Duration(seconds: 3),
+                                        boxShadow: const [],
+                                        removeElevation: false,
+                                        backgroundColor: Colors.grey[100], 
+                                        errorWidget: Container(
+                                        color: Colors.grey[300], 
+                                        child:  Text('${result!.code}'), 
+                                        ),
+                                        ),
+                          );
+                        } else {
+                          return Text('${result!.code}');
+                          // return Text("texrt");
+                        }
+                      } else {
+                        return Text('Scan a code');
+                      }
+                    }(),
+                  ),
+
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(height: 50,),
+            FloatingActionButton(onPressed: ()async {
+              await controller?.flipCamera();
+            },child: Icon(Icons.cameraswitch_rounded,color: Colors.white,),
+            mini: true,
+            backgroundColor: Colors.grey,),
+            FloatingActionButton(onPressed: ()async {
+              await controller?.toggleFlash();
+            },child: Icon(Icons.flashlight_on_outlined,color: Colors.white,),
+            mini: true,
+            backgroundColor: Colors.grey,),
+            SizedBox(height: 50,),
+            // FloatingActionButton(onPressed: () async{
+            //   await controller?.pauseCamera();
+            // },),
+            // FloatingActionButton(onPressed: () async{
+            //   await controller?.resumeCamera();
+            // },),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool isValidWebLink(String url) {
+ final uri = Uri.tryParse(url);
+//  return uri != null && uri.hasAbsolutePath;
+return true;
+}
+// bool isValidWebLink(String url) {
+//   final uri = Uri.tryParse(url);
+//   return uri != null && (uri.hasScheme && (uri.scheme == 'http' || uri.scheme == 'https'));
+// }
+// bool isValidLink(String url) {
+//  // Check if the URL is a valid URI with an absolute path
+//  bool isValidUri = Uri.tryParse(url)?.hasAbsolutePath ?? false;
+
+//  // Check if the URL starts with "www"
+//  bool startsWithWww = url.startsWith('www.');
+
+//  // Return true if either condition is met
+//  return isValidUri || startsWithWww;
+// }
+
+final style = TextStyle(
+  color: Colors.red,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  height: 1.375,
+);
+
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
